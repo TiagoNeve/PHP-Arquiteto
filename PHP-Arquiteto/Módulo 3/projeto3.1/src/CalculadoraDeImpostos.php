19e9a78184d8e1813d67b8a0a36b01d3c8bcd021
Tiago <rykarumstuler@gmail.com>
2022-05-18 03:32:11 +0000

    Finalizando Strategy

--- PHP-Arquiteto/Módulo 3/projeto3.1/src/CalculadoraDeImpostos.php
+++ PHP-Arquiteto/Módulo 3/projeto3.1/src/CalculadoraDeImpostos.php
@@ -5,6 +5,10 @@ namespace DesignPattern;
 use DesignPattern\Impostos\IImposto;
 
 // Exemplo de classe que tem grande chance de crescer para sempre
+/**
+ * Portanto, foi utilizado o Design Pattern Strategy nesse caso, que seria o ato de extrair os parâmetros para uma classe que implementa a mesma interface, mas com comportamentos diferentes
+ * Logo, cada classe implementa essa regra de negócio diferente e caso seja necessário ampliar, basta criar uma nova classe com a mesma interface, modificando apenas a regra de negócio.
+ */
 class CalculadoraDeImpostos
 {
   public function calcula(Orcamento $orcamento, IImposto $imposto): float
