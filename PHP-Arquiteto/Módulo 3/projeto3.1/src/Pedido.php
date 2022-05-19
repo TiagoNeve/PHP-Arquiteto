a3df0a16020436181ba1959e948c0d52ee1aeefb
Tiago <rykarumstuler@gmail.com>
2022-05-19 01:18:58 +0000

    Teste de functionamento

---
+++ PHP-Arquiteto/Módulo 3/projeto3.1/src/Pedido.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace DesignPattern;
+
+use DesignPattern\Orcamento;
+
+class Pedido
+{
+    public string $nomeCliente;
+    public \DateTimeInterface $dataFinalizacao;
+    public Orcamento $orcamento;
+}
